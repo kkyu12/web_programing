d1e58146626e2e7aa192e76e726a3fa09cfe3e9a
kkyu12 <lgj6799@gmail.com>
2022-06-09 13:05:19 +0900

    영어 모음 자음 체크

--- j/Word.java
+++ j/Word.java
@@ -1,38 +1,52 @@
+package Jun09th_oh_1;
+
 public class Word {
 	public static void main(String[] args) {
 		Word word = new Word("Hello World");
 		System.out.println(word.isVowel(0));
 		System.out.println(word.isVowel(1));
+		System.out.println(word.isVowel2(0));
+		System.out.println(word.isVowel2(1));
 	}
+
 	private String letters;
-	
+
 	public Word(String letters) {
 		this.letters = letters.toLowerCase();
 		// this.letters = new String(letters.toLowerCase());
 	}
-	
+
 	// i 번째 글자가 모음인지 a,i,u,e,o
 	public boolean isVowel(int i) {
 		boolean result;
 		switch (letters.charAt(i)) {
-		case 'a' :
+		case 'a':
 			result = true;
 			break;
-		case 'i' :
+		case 'i':
 			result = true;
 			break;
-		case 'e' :
+		case 'u':
 			result = true;
 			break;
-		case 'u' :
+		case 'e':
 			result = true;
 			break;
-		case 'o' :
+		case 'o':
 			result = true;
 			break;
-		default :
+		default:
 			result = false;
 		}
 		return result;
+	}
+	
+	public boolean isVowel2(int i) {
+		return "aioue".contains(letters.substring(i, i + 1));
+	}
+	
+	// i 번째 글자가 자음인지 a,i,u,e,o 제외
+	public boolean isConsonant(int i) {
+		return !("aioue".contains(letters.substring(i, i + 1)));
 	}
 }
